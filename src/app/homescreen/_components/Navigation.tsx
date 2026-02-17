2656f205d6c7e6436344971abc89c645924c5ccc
ariunzulm <munkhbatt.ariunzul@gmail.com>
2026-02-17 08:05:13 +0800

    search used useEffect

--- src/app/homescreen/_components/Navigation.tsx
+++ src/app/homescreen/_components/Navigation.tsx
@@ -1,59 +1,146 @@
 "use client";
-import { InputGroup, InputGroupInput } from "@/components/ui/input-group";
-import { ModeToggle } from "./ModeToggle";
-import { Search, X, ChevronDown, Clapperboard } from "lucide-react";
-import { useState } from "react";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
-export default function Navigation() {
-  const [isOpen, setIsOpen] = useState(false);
+import { ModeToggle } from "./ModeToggle";
+import {
+  Search,
+  ChevronDown,
+  Clapperboard,
+  Loader2,
+  SearchIcon,
+  X,
+  ArrowRight,
+} from "lucide-react";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+import Link from "next/link";
+import { Movie } from "@/lib/types";
+import { getSearchMovies } from "@/lib/api/search-movies";
+import { Button } from "@/components/ui/button";
+
+const Navigation = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onChangeSearchValue: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  useEffect(() => {
+    if (searchValue === "") {
+      setMovies([]);
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(true);
+
+    const timer = setTimeout(async () => {
+      const data = await getSearchMovies(searchValue);
+      console.log({ data });
+      setMovies(data.results);
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchValue]);
+
+  const handleClearSearch = () => {
+    setSearchValue("");
+    setMovies([]);
+    setIsSearchOpen(false);
+  };
 
   return (
     <header className="absolute inset-x-0 top-0 z-50 border border-b-zinc-200">
       <nav className="flex items-center justify-between px-6 py-4 lg:px-12 bg-zinc-800 bg-linear-to-b from-black to-transparent backdrop-blur-lg">
-        {isOpen && (
-          <ChevronDown className="border p-1.5 border-zinc-200 text-white rounded-xl w-8 h-8 transition-all cursor-pointer" />
-        )}
-        {!isOpen && (
-          <Link
-            href="/"
-            className="flex items-center gap-2 text-white font-bold text-xl hover:opacity-80 transition-opacity"
-          >
-            <div className="border border-gray-200 rounded-xl p-2 flex items-center justify-center">
-              <Clapperboard className="w-5 h-5 text-gray-200" />
-            </div>
-            <span className="text-2xl">MovieZ</span>
-          </Link>
-        )}
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-white font-bold text-xl hover:opacity-80 transition-opacity"
+        >
+          <div className="border border-gray-200 rounded-xl p-2 flex items-center justify-center">
+            <Clapperboard className="w-5 h-5 text-gray-200" />
+          </div>
+          <span className="text-2xl">MovieZ</span>
+        </Link>
+
         <div className="flex gap-3 items-center">
-          <InputGroup
-            className={cn(
-              "border relative md:min-w-95 border-zinc-200 rounded-xl md:flex mx-auto justify-center",
-              isOpen ? "flex-1" : "w-10",
-            )}
-          >
-            <div className="h-8 w-8 cursor-pointer flex items-center justify-center text-white  hover:text-white/80 ">
-              <Link href="/searchPage">
-                <Search className="w-6 h-6" />
-              </Link>
-            </div>
-
-            <InputGroupInput
-              placeholder="Search..."
+          <div className="relative">
+            <div
               className={cn(
-                "transition-all text-white",
-                isOpen ? "block w-full" : "hidden md:block",
+                "flex items-center w-fit border border-zinc-200 rounded-xl bg-zinc-700/50 transition-all",
+                isSearchOpen ? "w-64 md:w-96" : "w-10",
               )}
-            />
-          </InputGroup>
+            ></div>
+            <div
+              className="h-10 w-10 cursor-pointer flex items-center border border-gray-200 rounded-xl justify-center text-white hover:text-white/80"
+              onClick={() => setIsSearchOpen(!isSearchOpen)}
+            >
+              {isLoading && <Loader2 className="w-5 h-5 animate-spin" />}
+              {!isLoading && <SearchIcon className="w-5 h-5" />}
+            </div>
+          </div>
+          <input
+            type="text"
+            value={searchValue}
+            onChange={onChangeSearchValue}
+            placeholder="Search movies..."
+            className={cn(
+              "bg-transparent text-white outline-none border border-zinc-200 rounded-xl p-2 transition-all duration-200 placeholder:text-zinc-400",
+              isSearchOpen ? "w-full" : "w-0 opacity-0",
+            )}
+          />
+          {isSearchOpen && (
+            <button
+              onClick={handleClearSearch}
+              className="absolute right-2 text-white hover:text-white/80"
+            >
+              <X className="w-5 h-5" />
+            </button>
+          )}
+          <div className="absolute top-10 flex-col flex gap-1 bg-zinc-100">
+            {movies.slice(0, 4).map((movie) => (
+              <div key={movie.id} className="w-full flex p-6 justify-between">
+                <div className="flex gap-3">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path} `}
+                    alt={`${movie.title} poster`}
+                    className="w-16.75 h-25 object-cover rounded-md object-center shrink-0"
+                  />
+                  <div className="flex flex-col">
+                    <h2 className="text-base font-semibold text-muted-foreground relative">
+                      {movie.title}
+                    </h2>
 
-          <div className={cn(isOpen && "sm:hidden")}>
+                    <span className="flex gap-1 text-sm text-muted-foreground ">
+                      <span className=" text-yellow-400 fill-yellow-400">
+                        ★
+                      </span>
+                      {Math.floor(movie.vote_average * 10) / 10} (
+                      {movie.vote_count}
+                      votes)
+                    </span>
+
+                    <span className="text-sm">
+                      {movie.release_date.slice(0, 4)}
+                    </span>
+                  </div>
+                </div>
+                <Button className="w-fit text-sm self-end gap-1.5 hover:scale-105 transition-all duration-200 group/btn cursor-pointer">
+                  <span>See More</span>
+                  <ArrowRight className="w-3 h-3" />
+                </Button>
+              </div>
+            ))}
+          </div>
+          <div className={cn(isSearchOpen && "sm:hidden")}>
             <ModeToggle />
           </div>
         </div>
       </nav>
     </header>
   );
-}
+};
+
+export default Navigation;
