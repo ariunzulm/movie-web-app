9510d6a09799eb951c3ff8c840dac92c56027f7c
ariunzulm <munkhbatt.ariunzul@gmail.com>
2026-02-23 12:54:43 +0800

    pagination added

--- src/app/homescreen/_components/MovieHomeListings.tsx
+++ src/app/homescreen/_components/MovieHomeListings.tsx
@@ -14,7 +14,7 @@ const MovieHomeListings = async ({ title, movies }: MovieHomeListingsProps) => {
       <MovieListingTitles title={title} />
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {movies?.map((movie) => (
+        {movies?.slice(0, 8).map((movie) => (
           <Link key={movie.id} href={`/${movie.id}`} className="block">
             <MovieHomeCard
               movieName={movie.title}
